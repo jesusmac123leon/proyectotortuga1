bed5f3418110154b6fa09c1fd88219512012af56
jesus leon <leon-herido1@hotmail.com>
2019-09-27 00:07:12 -0500

    se realizo la conexion de la base de datos del compo cliente

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -4,7 +4,7 @@
 	date_default_timezone_set('America/Mexico_City');
 	$host="localhost";
 	$user="root";
-	$password="123456789";
+	$password="";
 	$name="misitio1";
 	$conn=@new mysqli($host,$user,$password,$name);
 	if(mysqli_connect_errno())
