fc210ebe2d55ec68a58d821804f514c5e8ebe3d9
jesus leon ortiz <leon-herido1@hotmail.com>
2019-11-11 13:34:29 -0600

    las actulizaciones de campo de menu

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
         $clave = hash('sha512', $clave);
         
         try{
-            $conexion = new PDO('mysql:host=localhost;dbname=logintortuga', 'root', '');
+            $conexion = new PDO('mysql:host=localhost;dbname=logintortuga','root', '');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
--- vistamenu.php
+++ vistamenu.php
@@ -3,24 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>cesion iniciada</title>
-    
+
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="css/style1.css">
-   
+
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
 
 
-   
+
    <div class="welcome">
-      
+
         <h1>Bienvenido, has logrado iniciar sesion</h1>
-       
+
    </div>
    <div class="welcome" >
     <table class="logo-title">
- 
+
         <tr  >
             <td>cliente</td>
             <td></td>
@@ -29,7 +29,7 @@
             <td>proveedor</td>
         </tr>
         <tr class class="logo-title">
-        
+
             <td ><a href="index2.php"><img src='./image/cliente.jpg' ></a></td>
             <td></td>
             <td><a><img src='./image/personal.png'></a></td>
@@ -44,24 +44,24 @@
             <td>insumo</td>
         </tr>
         <tr class class="logo-title">
-        
+
             <td ><a ><img src='./image/mesa3.png' ></a></td>
             <td></td>
-            <td><a><img src='./image/torta.jpg'></a></td>
+            <td><a href="tortas1/index.php"><img src='./image/torta.jpg'></a></td>
             <td></td>
             <td><a><img src='./image/insumo.png '></a></td>
         </tr>
- 
-        
- 
+
+
+
     </table>
     <div class="welcome">
-    
+
     <form  name="enviar_archivo_frm" method="post" action="subir-archivo.php" enctype="multipart/form-data">
 		<input type="file" name="archivo_fls" />
 <input type="submit" name="subir_btn" value="Subir Archivo" />
 	</form>
-  
+
    <a href="cerrar.php">Cerrar sesion</a>
 </div>
 </div>
