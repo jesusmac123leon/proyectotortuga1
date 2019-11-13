ed7d6c15899231e2e1f6309e0b1741e3744a41e5
jesus leon ortiz <leon-herido1@hotmail.com>
2019-11-12 22:40:35 -0600

    conexion a la base de datos remota en el campo de registro del login

--- register.php
+++ register.php
@@ -23,7 +23,7 @@
             $error .= '<i>Favor de rellenar todos los campos</i>';
         }else{
             try{
-                $conexion = new PDO('mysql:host=localhost;dbname=logintortuga', 'root', '');
+                $conexion = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_eaf83c356a363bf', 'b83c2d293c6136', '22fe08c4');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
