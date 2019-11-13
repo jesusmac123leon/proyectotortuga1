b8dbbd63a067564302ce969b50dae17f8176bafa
jesus leon ortiz <leon-herido1@hotmail.com>
2019-11-12 23:01:53 -0600

    se realizo una conexion de la base de datos el campo cliente

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -2,10 +2,10 @@
 <?php
 	date_default_timezone_set('GMT');
 	date_default_timezone_set('America/Mexico_City');
-	$host="localhost";
-	$user="root";
-	$password="";
-	$name="misitio1";
+	$host="us-cdbr-iron-east-05.cleardb.net";
+	$user="b83c2d293c6136";
+	$password="22fe08c4";
+	$name="heroku_eaf83c356a363bf";
 	$conn=@new mysqli($host,$user,$password,$name);
 	if(mysqli_connect_errno())
 	{
