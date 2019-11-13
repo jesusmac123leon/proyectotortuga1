cab293d1870f6b346da3e70219c4e3b752fd418e
jesus leon ortiz <leon-herido1@hotmail.com>
2019-11-12 22:30:32 -0600

    conexion a la base de datos de heroku

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     
     try{
-         $conexion = new PDO('mysql:host=localhost;dbname=logintortuga', 'root', '');
+        $conexion = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_eaf83c356a363bf', 'b83c2d293c6136', '22fe08c4');
     }catch(PDOException $prueba_error){
         echo "Error: " . $prueba_error->getMessage();
     }
--- login.php
+++ login.php
@@ -13,7 +13,7 @@
         $clave = hash('sha512', $clave);
         
         try{
-            $conexion = new PDO('mysql:host=localhost;dbname=logintortuga','root', '');
+            $conexion = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_eaf83c356a363bf', 'b83c2d293c6136', '22fe08c4');
             }catch(PDOException $prueba_error){
                 echo "Error: " . $prueba_error->getMessage();
             }
