121f9404c67a7e04e27678dc5b7e89d93c1ca74b
jesus leon ortiz <leon-herido1@hotmail.com>
2019-12-10 19:02:37 -0600

    base de datos

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,5 +1,5 @@
 
-<?php
+<?php/*
 	date_default_timezone_set('GMT');
 	date_default_timezone_set('America/Mexico_City');
 	$host="localhost";
@@ -10,5 +10,19 @@
 	if(mysqli_connect_errno())
 	{
 		exit();
-	}
+	}*/
+	
+
+date_default_timezone_set('GMT');
+date_default_timezone_set('America/Mexico_City');
+$host="us-cdbr-iron-east-05.cleardb.net";
+$user="b83c2d293c6136";
+$password="22fe08c4";
+$name="heroku_eaf83c356a363bf";
+$conn=@new mysqli($host,$user,$password,$name);
+if(mysqli_connect_errno())
+{
+	exit();
+}
+
 ?>
